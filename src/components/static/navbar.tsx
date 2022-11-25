bc4e202897db62d8ca110aa865681642c334dc89
Phuchon Chueapong <118894945+Rally8435@users.noreply.github.com>
2022-11-25 19:09:37 +0700

    Update Navbar Static Component and Add image exts into .gitignore

--- src/components/static/navbar.tsx
+++ src/components/static/navbar.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavbarStaticComponent() {
   return (
-    <nav>
-      <p>NavbarStaticComponent is works!</p>
+    <nav className="flex justify-center bg-[#170f33]">
+      <div className="flex justify-between items-center w-[calc(100%-40px)] max-w-7xl h-[60px] text-[#fff]">
+        <div>LOGO</div>
+        <div>Menu</div>
+      </div>
     </nav>
   )
 }
