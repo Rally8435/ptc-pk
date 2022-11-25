cf1d4eddddbb937029f6738f25acd256ba09ce83
Phuchon Chueapong <118894945+Rally8435@users.noreply.github.com>
2022-11-25 19:17:42 +0700

    Update Navbar Static Component
    Update LOGO Div Elemt, isn't release yet

--- src/components/static/navbar.tsx
+++ src/components/static/navbar.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Banner from '../../assets/banner.png';
 
 function NavbarStaticComponent() {
   return (
     <nav className="flex justify-center bg-[#170f33]">
-      <div className="flex justify-between items-center w-[calc(100%-40px)] max-w-7xl h-[60px] text-[#fff]">
-        <div>LOGO</div>
+      <div className="flex justify-between items-center w-[calc(100%-40px)] max-w-7xl text-[#fff]">
+        <Link to='/' title='หน้าแรกของ วิทยาลัยเทคโนโลยีภูเขียว' className="block py-[14px]">
+          <img src={Banner} alt="banner-ptc-logo" />
+        </Link>
+        
         <div>Menu</div>
       </div>
     </nav>
