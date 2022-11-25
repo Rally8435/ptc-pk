b2ea2810942ead41561d78b322f3189e3fcbc52b
Phuchon Chueapong <118894945+Rally8435@users.noreply.github.com>
2022-11-25 18:51:10 +0700

    Initial commit
    Initial React Typescript Library
    Initial TailwindCSS Framework
    Install React Router DOM

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <main className="bg-[black]">
+      <h1 className="text-[red]">HomePage is works!</h1>
+    </main>
+  )
+}
+
+export default HomePage
