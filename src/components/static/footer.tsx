d6507b94f3c360ac622c1489b49df5bfd0ef3e3e
Phuchon Chueapong <118894945+Rally8435@users.noreply.github.com>
2022-11-27 01:20:20 +0700

    Update Footer Static Component
    Setup prototype for Footer Static Component

--- src/components/static/footer.tsx
+++ src/components/static/footer.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 function FooterStaticComponent() {
   return (
-    <footer>
-      <p>FooterStaticComponent is works!</p>
+    <footer className="flex justify-center mt-[50px] py-3 bg-[#222]">
+      <div className="flex gap-x-3 w-[calc(100%-40px)] max-w-7xl">
+        <div className="flex-1 text-center rounded bg-[#f5f5f5]">XXX</div>
+        <div className="flex-1 text-center rounded bg-[#f5f5f5]">XXX</div>
+        <div className="flex-1 text-center rounded bg-[#f5f5f5]">XXX</div>
+      </div>
     </footer>
   )
 }
